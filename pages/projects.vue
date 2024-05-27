b53f3af5cdc339a6cd7baee6dedeeed7713b3142
Islati <islati.sk@gmail.com>
2024-05-26 21:56:41 -0300

    Update tailwind screen sizes, and include mobile friendly project displays

--- pages/projects.vue
+++ pages/projects.vue
@@ -7,14 +7,15 @@ const projects = ref([
     name: 'Spellbound Saga',
     description: 'Card Game to save the world from litter.',
     imageUrl: '/imgs/projects/spellbound-saga.png',
-    footerUrl: 'https://spellboundsaga.com'
+    footerUrl: 'https://spellboundsaga.com',
+    imageClasses: 'object-contain xs:h-4/5 sm:h-36 md:h-42 lg:h-48 w-96'
   },
   {
     name: 'Skreet Media Inc.',
     description: 'Canadian Urban Record Label',
     imageUrl: '/imgs/projects/skreet-media-inc-white.png',
     footerUrl: 'https://skreetmedia.com',
-    imageClasses: 'bg-black hover:filter hover:invert object-contain xs:h-12 sm:h-36 md:h-42 lg:h-48 w-96',
+    imageClasses: 'bg-black hover:filter hover:invert object-contain xs:h-4/5 sm:h-36 md:h-42 lg:h-48 w-96',
   },
   {
     name: 'Wogwon Society',
@@ -22,7 +23,7 @@ const projects = ref([
     imageUrl: '/imgs/logo-dark.png',
     footerUrl: 'https://wogwon.com/about',
     coverAndFit: true,
-    imageClasses: 'bg-white hover:filter hover:invert object-contain sm:h-36 md:h-42 lg:h-48 w-96',
+    imageClasses: 'bg-white hover:filter hover:invert object-contain xs:h-4/5 sm:h-36 md:h-42 lg:h-48 w-96',
   }
 ]);
 
@@ -54,11 +55,20 @@ if (!props.component) {
     </section>
 
     <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-      <Project v-for="project in projects" :key="project.name" :title="project.name" :description="project.description"
-               :imageUrl="project.imageUrl" :footerUrl="project.footerUrl" :imageClasses="project.imageClasses"/>
+      <Project
+          v-for="project in projects"
+          :key="project.name"
+          :title="project.name"
+          :description="project.description"
+          :imageUrl="project.imageUrl"
+          :footerUrl="project.footerUrl"
+          :imageClasses="project.imageClasses"
+          />
     </section>
   </main>
 </template>
 
+
 <style scoped>
+
 </style>
