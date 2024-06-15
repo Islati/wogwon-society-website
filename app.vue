5fc75e132c816ec9e3d5087303242b8d9aeec89e
Islati <islati.sk@gmail.com>
2024-06-15 18:08:49 -0300

    Optimized to 98 / 100 so far.

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  meta: [
+    {'http-equiv': 'Cache-Control', content: 'public, max-age=31536000, immutable'}
+  ]
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,59 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  css: [`assets/css/styles.css`],
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', "@nuxt/image"],
-  nitro: {
-    routeRules: {
-      "/img/**": { headers: { 'cache-control': `public,max-age=${32536000},s-maxage=${32536000}` } },
-      "/_nuxt/**": { headers: { 'cache-control': `public,max-age=${32536000},s-maxage=${32536000}` } },
+    ssr: false,
+    css: [`assets/css/styles.css`],
+    devtools: {enabled: true},
+    modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', "@nuxt/image"],
+    nitro: {
+        routeRules: {
+            "/img/**": {headers: {'cache-control': `public,max-age=${32536000},s-maxage=${32536000}`}},
+            "/_nuxt/**": {headers: {'cache-control': `public,max-age=${32536000},s-maxage=${32536000}`}},
+        },
+        publicAssets: [
+            {
+                baseURL: "video",
+                dir: "public/video",
+                maxAge: 60 * 60 * 24 * 365,
+            },
+            {
+                baseURL: "images",
+                dir: "public/imgs",
+                maxAge: 60 * 60 * 24 * 365,
+            },
+            {
+                baseURL: "animations",
+                dir: "public/animations",
+                maxAge: 60 * 60 * 24 * 365,
+            },
+        ],
+        compressPublicAssets: {
+            brotli: true,
+            gzip: true,
+        },
     },
-    publicAssets: [
-      {
-        baseURL: "video",
-        dir: "public/video",
-        maxAge: 60 * 60 * 24 * 365,
-      },
-      {
-        baseURL: "images",
-        dir: "public/imgs",
-        maxAge: 60 * 60 * 24 * 365,
-      },
-      {
-        baseURL: "animations",
-        dir: "public/animations",
-        maxAge: 60 * 60 * 24 * 365,
-      },
-    ],
-    compressPublicAssets: {
-      brotli: true,
-      gzip: true,
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
     },
-  },
-  shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
-    prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
-    componentDir: './components/ui'
-  },
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      htmlAttrs: {
-          lang: 'en'
-      },
-      meta: [
-        { name: 'description', content: 'Wogwon Society - Promoting Music, Arts, and Culture' },
-        { name: 'keywords', content: 'Wogwon Society, music, arts, culture, community events, non-profit' }
-      ]
-    },
-    // pageTransition: { name: 'page', mode: 'out-in' }
-  }
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            htmlAttrs: {
+                lang: 'en'
+            },
+            meta: [
+                {name: 'description', content: 'Wogwon Society - Promoting Music, Arts, and Culture'},
+                {name: 'keywords', content: 'Wogwon Society, music, arts, culture, community events, non-profit'},
+                // {name: 'Cache-Control', content: 'public, max-age=31536000, immutable'}
+            ]
+        },
+        // pageTransition: { name: 'page', mode: 'out-in' }
+    }
 })
